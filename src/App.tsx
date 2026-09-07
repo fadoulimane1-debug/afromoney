6335e1edb0a6d1ca0c7fb53ef603cae44dfe4b26
fadoulimane1-debug <fadoulimane1@gmail.com>
2026-09-07 12:17:25 +0200

    Refactor imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { migrateLocalToCloudIfEmpty, pullAllFromCloud } from '@/lib/cloudSync';
+import { pullAllFromCloud, isCloudSyncEnabled } from '@/lib/cloudSync';
 import { isCloudSyncEnabled } from '@/lib/cloudConfig';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from '@/components/Layout';
